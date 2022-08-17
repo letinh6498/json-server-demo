ab41f18048d8250635cf9c33081eadcbcab3b4bd
tinh le <letinh06041998@gmail.com>
2022-08-17 09:37:27 +0700

    tinh le up h t m l

--- main.js
+++ main.js
@@ -56,19 +56,25 @@ router.render = (req, res) => {
 // Use default router
 server.use('/api', router);
 
-var http = require('http'),
-    fs = require('fs');
+// var http = require('http'),
+//     fs = require('fs');
 
 
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(3000);
+// fs.readFile('./index.html', function (err, html) {
+//     if (err) {
+//         throw err; 
+//     }       
+//     http.createServer(function(request, response) {  
+//         response.writeHeader(200, {"Content-Type": "text/html"});  
+//         response.write(html);  
+//         response.end();  
+//     }).listen(3000);
+// });
+
+
+
+server.get('/tinh', function(request, response){
+    response.sendFile('index.html');
 });
 
 // Start server
