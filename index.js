0fd253fcc7c554d0ec73d3cf06e74fa095627071
tinh le <letinh06041998@gmail.com>
2022-08-17 10:05:07 +0700

    h

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express')
 const app = express()
 
 
-app.get('/', function(request, response){
-    response.sendFile('index.html');
+var path    = require("path");
+
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
 });
+
 app.get('/hello', (req, res) => {
   res.send('Hello World!')
 })
