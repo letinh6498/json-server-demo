cac0ab2ddb6a2e7a7aa97dd49365517c21e34723
tinh le <letinh06041998@gmail.com>
2022-08-17 09:54:56 +0700

    h

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const app = express()
-const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-const PORT = env.process.PORT || 3000
+port = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
 })
